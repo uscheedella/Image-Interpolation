44c4d221972144642ee7c710ae20416db4bb2119
Russell J. Hewett <rhewett@vt.edu>
2019-01-31 13:59:17 -0500

    Added problem setup

--- labs/lab02_sol/code/vectors.c
+++ labs/lab02_sol/code/vectors.c
@@ -6,5 +6,20 @@ typedef struct Vector3D_tag {
 
 int main()
 {
+
+    float alpha = 0.25;
+    float beta = 0.56;
+
+    Vector3D vec_x;
+    Vector3D vec_y;
+
+    vec_x.x = 1.0;
+    vec_x.y = 1.5;
+    vec_x.z = 2.3;
+
+    vec_y.x = 0.01;
+    vec_y.y = 5.0;
+    vec_y.z = 17.1717;
+
     return 0;
 }
