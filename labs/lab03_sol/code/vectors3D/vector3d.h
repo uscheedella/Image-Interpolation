f45c3bb777bc731095042f30a5227e75e1c029b3
Russell J. Hewett <rhewett@vt.edu>
2019-02-02 15:59:55 -0500

    Completed 2.a.ii
    Separated vectors.c into header, implementation, and driver files.

--- labs/lab03_sol/code/vectors3D/vector3d.h
+++ labs/lab03_sol/code/vectors3D/vector3d.h
@@ -1,6 +1,5 @@
-#include <stdio.h>
-
-#include <math.h>
+#ifndef __VECTOR3D_H__
+#define __VECTOR3D_H__
 
 typedef struct Vector3D_tag {
     float x;
@@ -8,65 +7,11 @@ typedef struct Vector3D_tag {
     float z;
 } Vector3D;
 
-float norm(Vector3D v) {
+float norm(Vector3D v);
 
-    float length;
+Vector3D axpy(float alpha, Vector3D vx, Vector3D vy);
 
-    length = sqrt(pow(v.x, 2.0) + pow(v.y, 2.0) + pow(v.z, 2.0));
+float inner_product(Vector3D vx, Vector3D vy);
 
-    return length;
-}
+#endif
 
-Vector3D axpy(float alpha, Vector3D vx, Vector3D vy) {
-    Vector3D vz;
-
-    vz.x = alpha*vx.x + vy.x;
-    vz.y = alpha*vx.y + vy.y;
-    vz.z = alpha*vx.z + vy.z;
-
-    return vz;
-}
-
-float inner_product(Vector3D vx, Vector3D vy) {
-    return vx.x*vy.x + vx.y*vy.y + vx.z*vy.z;
-}
-
-int main()
-{
-
-    float alpha = 0.25;
-    float beta = 0.56;
-
-    Vector3D vec_x;
-    Vector3D vec_y;
-
-    vec_x.x = 1.0;
-    vec_x.y = 1.5;
-    vec_x.z = 2.3;
-
-    vec_y.x = 0.01;
-    vec_y.y = 5.0;
-    vec_y.z = 17.1717;
-
-    
-    float m;
-    m = norm(vec_x);
-
-    printf("L2 Q2a) norm(vec_x) = %f\n", m);
-   
-    Vector3D vec_z1;
-    Vector3D vec_z2;
-
-    vec_z1 = axpy(alpha, vec_x, vec_y);
-    printf("L2 Q2b) axpy(alpha, vec_x, vec_y) = [%f, %f, %f]^T\n", vec_z1.x, vec_z1.y, vec_z1.z);
-
-    vec_z2 = axpy(alpha, vec_y, vec_y);
-    printf("L2 Q2c) axpy(beta, vec_y, vec_y) = [%f, %f, %f]^T\n", vec_z2.x, vec_z2.y, vec_z2.z);
-    
-    float a;
-    a = inner_product(vec_x, vec_y);
-    printf("L2 Q2d) inner_product(vec_x, vec_y) = %f\n", a);
-
-
-    return 0;
-}
--- labs/lab03_sol/code/vectors3D/vector_driver.c
+++ labs/lab03_sol/code/vectors3D/vector_driver.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 
-#include <math.h>
-
-typedef struct Vector3D_tag {
-    float x;
-    float y;
-    float z;
-} Vector3D;
-
-float norm(Vector3D v) {
-
-    float length;
-
-    length = sqrt(pow(v.x, 2.0) + pow(v.y, 2.0) + pow(v.z, 2.0));
-
-    return length;
-}
-
-Vector3D axpy(float alpha, Vector3D vx, Vector3D vy) {
-    Vector3D vz;
-
-    vz.x = alpha*vx.x + vy.x;
-    vz.y = alpha*vx.y + vy.y;
-    vz.z = alpha*vx.z + vy.z;
-
-    return vz;
-}
-
-float inner_product(Vector3D vx, Vector3D vy) {
-    return vx.x*vy.x + vx.y*vy.y + vx.z*vy.z;
-}
+#include "vector3d.h"
 
 int main()
 {
