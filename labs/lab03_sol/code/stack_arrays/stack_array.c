2dfa87fdb0f61007250c154ca11e66af2e5140ec
Russell J. Hewett <rhewett@vt.edu>
2019-02-05 09:08:44 -0500

    Completed Lab 03 2.a.viii

--- labs/lab03_sol/code/stack_arrays/stack_array.c
+++ labs/lab03_sol/code/stack_arrays/stack_array.c
@@ -18,5 +18,26 @@ int main(){
     printf("Sum 0-127  = %d\n", sum);
     printf("Mean 0-127 = %f\n", mean);
 
+    // First entry
+    int* ptr0 = arr;
+    // 33rd entry
+    int* ptr32 = &arr[32];
+    // 65th entry
+    int* ptr64 = &arr[64];
+    // 97th entry
+    int* ptr96 = &arr[96];
+
+    printf("Sum 0-31  = %d\n", sum_array(ptr0, 32));
+    printf("Mean 0-31 = %f\n", mean_array(ptr0, 32));
+
+    printf("Sum 32-63  = %d\n", sum_array(ptr32, 32));
+    printf("Mean 32-63 = %f\n", mean_array(ptr32, 32));
+    
+    printf("Sum 64-95  = %d\n", sum_array(ptr64, 32));
+    printf("Mean 64-95 = %f\n", mean_array(ptr64, 32));
+    
+    printf("Sum 96-127  = %d\n", sum_array(ptr96, 32));
+    printf("Mean 96-127 = %f\n", mean_array(ptr96, 32));
+    
     return 0;
 }
