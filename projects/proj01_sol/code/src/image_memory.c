c162d2376630853d3bc8deb41d093f152b9eb694
Russell J. Hewett <rhewett@vt.edu>
2019-02-11 19:34:48 -0500

    proj01 sol added float image allocators

--- projects/proj01_sol/code/src/image_memory.c
+++ projects/proj01_sol/code/src/image_memory.c
@@ -106,3 +106,107 @@ int deallocate_IntensityImage8(IntensityImage8* image)
 
     return 0;
 }
+
+
+
+
+/*
+* Nullifies a single channel (intensity), float image struct.
+*
+* This should always be called after an instance is created.
+* There is no mechanism to know if it points to anything, so
+* care should be taken.
+*
+* Arguments:
+*   image: pointer to the image to be nullified
+*
+* Returns:
+*   error code: 0 on success
+*/
+
+int nullify_IntensityImageFloat(IntensityImageFloat* image)
+{
+    image->width = 0;
+    image->height = 0;
+    image->data = NULL;
+
+    return 0;   
+}
+
+
+/*
+* Allocates a single channel (intensity), float image struct.
+*
+* The allocated array is sized width*height.
+*
+* Arguments:
+*   image: pointer to the image to be allocated
+*   width: image width
+*   height: image height
+*
+* Returns:
+*   error code: 0 on success, 1 on failure
+*/
+
+int allocate_IntensityImageFloat(IntensityImageFloat* image, int width, int height)
+{
+    int n_pixels;
+
+    image->width = width;
+    image->height = height;
+
+    n_pixels = width*height;
+
+    image->data = malloc(n_pixels*sizeof(float));
+
+    if(image->data == NULL){
+        fprintf(stderr, "Error allocating image data.\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+
+/*
+* Initlaizes a single channel (intensity), float image to 0.
+*
+* Arguments:
+*   image: pointer to the image to be initialized
+*
+* Returns:
+*   error code: 0 on success
+*/
+
+int initialize_IntensityImageFloat(IntensityImageFloat* image)
+{
+    int n_pixels = (image->width)*(image->height);
+
+    for(int i=0;i<n_pixels;i++)
+        image->data[i] = 0.0;
+
+    return 0;
+}
+
+
+/*
+* Deallocates a single channel (intensity), float image.
+*
+* Arguments:
+*   image: pointer to the image to be deallocated
+*
+* Returns:
+*   error code: 0 on success
+*/
+
+int deallocate_IntensityImageFloat(IntensityImageFloat* image)
+{
+    image->width = 0;
+    image->height = 0;
+
+    free(image->data);
+
+    image->data = NULL;
+
+    return 0;
+}
