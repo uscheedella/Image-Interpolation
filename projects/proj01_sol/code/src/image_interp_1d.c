fda057b7ec3149360d132bd856818f4ca1b9238c
Russell J. Hewett <rhewett@vt.edu>
2019-02-12 14:14:45 -0500

    proj01 sol cleaned up variables in 1d interp code

--- projects/proj01_sol/code/src/image_interp_1d.c
+++ projects/proj01_sol/code/src/image_interp_1d.c
@@ -17,11 +17,11 @@ int compute_1d_downsample_size(int N, int C, int K){
 
 int interp1d_downsample(int C, int K, MImageF* in, MImageF* out){
 
-    int i, ihat;
+    int i, i_hat;
 
-    for(int ihat=0; ihat < out->width; ihat++){
-        i = C + ihat*K;
-        out->data[ihat] = in->data[i];
+    for(int i_hat=0; i_hat < out->width; i_hat++){
+        i = C + i_hat*K;
+        out->data[i_hat] = in->data[i];
     }
 
     return 0;
@@ -30,31 +30,33 @@ int interp1d_downsample(int C, int K, MImageF* in, MImageF* out){
 
 int interp1d_nearest(MImageF* in, MImageF* out){
 
-    int i, ihat;
-    float h, hhat;
-    float xhat;
+    int i, i_hat;
+    float h, h_hat;
+    float x_hat;
 
     const float a = 0.0;
     const float b = 1.0;
 
     h = (b - a) / (in->width-1.0);
-    hhat = (b - a) / (out->width-1.0);
+    h_hat = (b - a) / (out->width-1.0);
 
-    for(int ihat=0; ihat < out->width; ihat++){
-        xhat = a + ihat*hhat;
-        i = (int)floor((xhat-a)/h);
+    for(int i_hat=0; i_hat < out->width; i_hat++){
+        x_hat = a + i_hat*h_hat;
+        i = (int)floor((x_hat-a)/h);
 
-        out->data[ihat] = in->data[i];
+        out->data[i_hat] = in->data[i];
     }
+
+    return 0;
 }
 
 
 
 int interp1d_linear(MImageF* in, MImageF* out){
 
-    int i, ihat;
-    float h, hhat;
-    float xhat;
+    int i, i_hat;
+    float h, h_hat;
+    float x_hat;
     float xi, xip1;
     float w;
 
@@ -62,25 +64,27 @@ int interp1d_linear(MImageF* in, MImageF* out){
     const float b = 1.0;
 
     h = (b - a) / (float)(in->width-1);
-    hhat = (b - a) / (out->width-1.0);
+    h_hat = (b - a) / (out->width-1.0);
 
-    for(int ihat=0; ihat < out->width; ihat++){
-        xhat = a + ihat*hhat;
-        i = (int)floor((xhat-a)/h);
+    for(int i_hat=0; i_hat < out->width; i_hat++){
+        x_hat = a + i_hat*h_hat;
+        i = (int)floor((x_hat-a)/h);
         xi = a + i*h;
         xip1 = a + (i+1)*h;
 
-        w = (xip1 - xhat) / h;
+        w = (xip1 - x_hat) / h;
 
-        out->data[ihat] = w*in->data[i] + (1-w)*in->data[i+1];
+        out->data[i_hat] = w*in->data[i] + (1-w)*in->data[i+1];
     }
+
+    return 0;
 }
 
 int interp1d_cubic(MImageF* in, MImageF* out){
 
-    int i, ihat;
-    float h, hhat;
-    float xhat;
+    int i, i_hat;
+    float h, h_hat;
+    float x_hat;
     float xk;
     float i_temp;
 
@@ -92,12 +96,12 @@ int interp1d_cubic(MImageF* in, MImageF* out){
     const float b = 1.0;
 
     h = (b - a) / ((float)in->width-1.0);
-    hhat = (b - a) / ((float)out->width-1.0);
+    h_hat = (b - a) / ((float)out->width-1.0);
 
-    for(int ihat=0; ihat < out->width; ihat++){
-        xhat = a + ihat*hhat;
+    for(int i_hat=0; i_hat < out->width; i_hat++){
+        x_hat = a + i_hat*h_hat;
 
-        i = (int)floor((xhat-a)/h);
+        i = (int)floor((x_hat-a)/h);
         i = max(i, 1);
         i = min(i, in->width);
 
@@ -114,10 +118,12 @@ int interp1d_cubic(MImageF* in, MImageF* out){
 
         linear_solve(4, A, rhs, coeffs);
 
-        out->data[ihat] = 0.0;
+        out->data[i_hat] = 0.0;
         for(int k=0; k<4; k++){
-            out->data[ihat] += coeffs[k]*pow(xhat, (float)(3-k));
+            out->data[i_hat] += coeffs[k]*pow(x_hat, (float)(3-k));
         }
     }
+
+    return 0;
 }
 
