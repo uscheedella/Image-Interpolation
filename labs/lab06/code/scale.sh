4e79146937c81124d6a7388c842098e93a84503d
stimmel <stimmel@princeton.edu>
2019-02-26 18:10:26 -0500

    Moved spurious build rule into script

--- labs/lab06/code/scale.sh
+++ labs/lab06/code/scale.sh
@@ -2,12 +2,12 @@ echo "Doing scaling study with 1, 2, 4, 8, and 16 cores."
 make clean
 make omp
 export OMP_NUM_THREADS=1
-make run_omp
+./bin/pi_omp 1000000000
 export OMP_NUM_THREADS=2
-make run_omp
+./bin/pi_omp 1000000000
 export OMP_NUM_THREADS=4
-make run_omp
+./bin/pi_omp 1000000000
 export OMP_NUM_THREADS=8
-make run_omp
+./bin/pi_omp 1000000000
 export OMP_NUM_THREADS=16
-make run_omp
+./bin/pi_omp 1000000000
