cca88a7f2ec1f8493b2e0f962629164dbb33218f
Russell J. Hewett <rhewett@vt.edu>
2019-03-04 10:09:15 -0500

    lab05 sol added initial files

---
+++ labs/lab05_sol/code/time_col_orient.sh
@@ -0,0 +1,11 @@
+echo "experiment, M, N, nTrials, col total(s), col avg(s)"
+./bin/colOrient 10 10
+./bin/colOrient 20 20
+./bin/colOrient 40 40
+./bin/colOrient 80 80
+./bin/colOrient 160 160
+./bin/colOrient 320 320
+./bin/colOrient 640 640
+./bin/colOrient 1280 1280
+./bin/colOrient 2560 2560
+./bin/colOrient 5120 5120
