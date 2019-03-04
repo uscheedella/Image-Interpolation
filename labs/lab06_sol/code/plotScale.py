7b0bfd23a0c7942c69eb2b99bc4977488a8c88ab
Russell J. Hewett <rhewett@vt.edu>
2019-03-04 14:54:47 -0500

    lab06 sol improved plot tool

--- labs/lab06_sol/code/plotScale.py
+++ labs/lab06_sol/code/plotScale.py
@@ -1,22 +1,58 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from numpy import log as ln
 
-times = []
+times_laptop = []
+times_cascades = []
 cores = []
 
-with open("scale.txt", "r") as f:
+with open("scale_laptop.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
         line = line.split(" ")
         if line[0] == "dt":
-            times.append(float(line[2]))
+            times_laptop.append(float(line[2]))
     f.close()
 
-n = len(times)
+with open("scale_cascades.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.split(" ")
+        if line[0] == "dt":
+            times_cascades.append(float(line[2]))
+    f.close()
+
+n = len(times_laptop)
 for i in range(n):
     cores.append(2**i)
     
-plt.plot(cores,times)
+plt.figure()
+plt.loglog(cores, times_laptop, "-o", label="laptop", linewidth=2)
+plt.loglog(cores, times_cascades, "-o", label="cascades", linewidth=2)
+plt.legend()
 plt.xlabel("Cores")
 plt.ylabel("Compute Time (s)")
+
+
+converge = []
+
+with open("converge.txt", "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.split(" ")
+        if line[0] == "estPi":
+            converge.append(float(line[2]))
+    f.close()
+
+n = 10**np.arange(len(converge),dtype=np.int64) * 100
+converge = np.array(converge)
+
+relerr = np.abs(converge - np.pi)/np.pi
+
+plt.figure()
+plt.loglog(n, relerr, "-o", label="Relative Error", linewidth=2)
+plt.title("Relative error wrt $\pi$")
+plt.xlabel("N Samples")
+plt.ylabel("Error")
+
 plt.show()
