8c170983e947214a41f4202b9f007c1960731c2a
Russell J. Hewett <rhewett@vt.edu>
2019-03-04 14:12:15 -0500

    lab06 sol init

---
+++ labs/lab06_sol/code/scale.sh
@@ -0,0 +1,13 @@
+echo "Doing scaling study with 1, 2, 4, 8, and 16 cores."
+make clean
+make omp
+export OMP_NUM_THREADS=1
+./bin/pi_omp 1000000000
+export OMP_NUM_THREADS=2
+./bin/pi_omp 1000000000
+export OMP_NUM_THREADS=4
+./bin/pi_omp 1000000000
+export OMP_NUM_THREADS=8
+./bin/pi_omp 1000000000
+export OMP_NUM_THREADS=16
+./bin/pi_omp 1000000000
